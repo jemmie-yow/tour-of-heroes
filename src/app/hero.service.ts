eccc9f57b30e991e10d11b73447d465bfc34a868
Jemwel Odo <jodo@magaya.com>
2025-03-20 17:16:47 +0800

    added proxy

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -8,7 +8,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
   providedIn: 'root',
 })
 export class HeroService {
-  private heroesUrl = 'http://localhost:8080/api/hero';
+  private heroesUrl = '/api/hero';
   private httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
   };
